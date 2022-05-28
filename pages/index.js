8c117df746a80875a489823b9ee3e2a0ab158f4e
Adham-Niazy <adhamniazy@gmail.com>
2022-05-28 11:27:43 +0200

    Designing Home Page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
 import React from "react";
 
+import { Product, HeroBanner, FooterBanner } from "../components";
+
 const Home = () => {
   return (
     <>
-      HeroBanner
-      <div>
+      <HeroBanner />
+      <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div>{["Product 1", "Product 2"].map((product) => product)}</div>
-      Footer
+      <div className="products-container">
+        {["Product 1", "Product 2"].map((product) => product)}
+      </div>
+
+      <FooterBanner />
     </>
   );
 };
