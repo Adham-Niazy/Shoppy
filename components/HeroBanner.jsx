28c390e2d1fd9ecef005039951ad692b4422c2a4
Adham-Niazy <adhamniazy@gmail.com>
2022-05-28 11:33:56 +0200

    HeroBanner UI

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
-  )
-}
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">SMALL TEXT</p>
+        <h3>MID TEXT</h3>
+        <img src="" alt="headphones" className="hero-banner-image" />
 
-export default HeroBanner
+        <div>
+          <Link href={"/product/id"}>
+            <button type="button">BUTTON TEXT</button>
+          </Link>
+
+          <div className="desc">
+            <h5>Description</h5>
+            <p>DESCRIPTION</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroBanner;
