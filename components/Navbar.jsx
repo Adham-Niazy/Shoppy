b67f3b7e28ee516eac08a756cef086d363193fb5
Adham-Niazy <adhamniazy@gmail.com>
2022-05-30 11:05:20 +0200

    Navbar Component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href={"/"}>Shoppy</Link>
+      </p>
+      <button className="cart-icon" type="button" onClick={""}>
+        <AiOutlineShopping />
+        <span className="cart-item-qty">1</span>
+      </button>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
